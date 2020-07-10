76a8dec15bbb5b46abede76fa81d8097d0bef162
Ronny Nguyen <42280743+nguryu@users.noreply.github.com>
2020-07-10 12:27:29 -0400

    Add files via upload

--- sgr_project/search_sgr.py
+++ sgr_project/search_sgr.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from gbm.time import Met
 from gbm.data.poshist import PosHist
 
+search_date = '2020-06-19'
+
 # ================================================== #
 # *** GBM/SGR CATALOGUE *** #
 # ================================================== #
@@ -20,7 +22,7 @@ bat_exp = []
 archv_date = []
 
 # Read in data.
-with open('/Users/RonnyNguyen/Desktop/FermiGBMSummer2020/data_dir/BrowseTargets.txt', 'r') as data_file:
+with open('/Users/RonnyNguyen/Desktop/FermiGBMSummer2020/data_dir/BrowseTargetsJune619.txt', 'r') as data_file:
     for line in data_file:
         line = line.strip()  # Remove whitespace.
         if not line:  # Skip empty lines.
@@ -41,46 +43,63 @@ with open('/Users/RonnyNguyen/Desktop/FermiGBMSummer2020/data_dir/BrowseTargets.
 # Search for SGR sources.
 sgr_name = []
 sgr_obsid = []
+sgr_start_time = []
 sgr_ra = []
 sgr_dec = []
-
 for i in range(len(name)):
-    if name[i][0:3] == 'SGR':
+    if name[i][0:3] == 'SGR' and start_time[i][0:10] == search_date:
         sgr_name.append(name[i])
         sgr_obsid.append(obsid[i])
+        sgr_start_time.append(start_time[i])
         sgr_ra.append(ra[i])
         sgr_dec.append(dec[i])
 
 # Convert RA and DEC to decimal.
-sgr_ra_deg = []
-sgr_dec_deg = []
-
+ra_deg = []
+dec_deg = []
 for i in range(len(sgr_name)):
     ra_conv = (float(sgr_ra[i][0:2]) + float(sgr_ra[i][3:5])/60. + float(sgr_ra[i][6:11])/3600.)*360/24.
     dec_conv = float(sgr_dec[i][0:3]) + float(sgr_dec[i][4:6])/60. + float(sgr_dec[i][7:12])/3600.
-    sgr_ra_deg.append(round(ra_conv, 2))
-    sgr_dec_deg.append(round(dec_conv, 2))
+    ra_deg.append(round(ra_conv, 2))
+    dec_deg.append(round(dec_conv, 2))
 
 # ================================================== #
 # *** POSITION HISTORY FILE *** #
 # ================================================== #
 # Read in data.
-poshist = PosHist.open("/Users/RonnyNguyen/Desktop/FermiGBMSummer2020/data_dir/glg_poshist_all_200619_v00.fit")
+poshist = PosHist.open('/Users/RonnyNguyen/Desktop/FermiGBMSummer2020/data_dir/glg_poshist_all_200619_v00.fit')
 
-# Convert UTC time to mission elapsed time (MET, seconds) for Fermi.
-time = "2020-06-19T15:20:36"
-met = Met.from_iso(time).met
+# Convert to correct time format for from_iso() function.
+time_conv = []
+for i in range(len(sgr_start_time)):
+    time_conv.append(sgr_start_time[i][0:10] + 'T' + sgr_start_time[i][11:19])
 
-# Check if inside SAA +/- 10 sec pad.
-# SAA = South Atlantic Anomaly where Fermi-GBM turns off due to the high particle flux.
-in_saa = poshist.get_saa_passage(met) | poshist.get_saa_passage(met + 10) | poshist.get_saa_passage(met - 10)
+# Check if source is visible to Fermi or not.
+within_saa = []
+not_visible = []
+visible = []
+for i in range(len(sgr_name)):
+    # Convert UTC time to mission elapsed time (MET in seconds) for Fermi.
+    time = time_conv[i]
+    met = Met.from_iso(time).met
 
-# Check if true location is occulted.
-occulted = poshist.location_visible(sgr_ra_deg, sgr_dec_deg, met) == False
+    # Check if inside SAA +/- 10 sec pad.
+    # SAA = South Atlantic Anomaly where Fermi-GBM turns off due to the high particle flux.
+    in_saa = poshist.get_saa_passage(met) | poshist.get_saa_passage(met + 10) | poshist.get_saa_passage(met - 10)
 
-if in_saa:
-  print("Fermi GBM is in SAA +/- 10 sec")
-elif occulted:
-  print("Position not visible to Fermi GBM")
-else:
-  print("Source should be visible to Fermi GBM")
+    # Check if true location is occulted.
+    occulted = poshist.location_visible(ra_deg[i], dec_deg[i], met) == False
+
+    if in_saa:
+      print("Fermi GBM is in SAA +/- 10 sec")
+      within_saa.append(sgr_name[i].strip())  # strip() to remove extra white space.
+    elif occulted:
+      print("Position not visible to Fermi GBM")
+      not_visible.append(sgr_name[i].strip())
+    else:
+      print("Source should be visible to Fermi GBM")
+      visible.append(sgr_name[i].strip())
+
+print("Within SAA:\t", within_saa)
+print("Not visible:\t", not_visible)
+print("Visible:\t", visible)
